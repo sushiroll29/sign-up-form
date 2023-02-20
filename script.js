8a3b4e3e4a510d72e1b29c28d393b8f273bb94b0
Ale <ale.surdu29@gmail.com>
2023-02-20 11:55:45 +0100

    Display message when passwords do not match

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-const form = document.querySelector('form');
-// const submitButton = document.querySelector('button');
-
 function checkPassword(){
     const password = document.querySelector('#password');
     const confirmPassword = document.querySelector('#confirm-password');
+    const messageBox = document.querySelector('.password-match');
     if(password.value !== confirmPassword.value){
         password.style.cssText = 'box-shadow: 0 0 0 2pt red;';
         confirmPassword.style.cssText = 'box-shadow: 0 0 0 2pt red;';
+        messageBox.textContent = '*Passwords do not match'
         return false;
     } else {
         return true;
     }
 
 }
-
