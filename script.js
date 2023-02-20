fca6304edcd240e5faa81c39ec87c1a6c0b2545d
Ale <ale.surdu29@gmail.com>
2023-02-20 12:08:08 +0100

    Remove red outline when retyping password

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirm-password');
+const messageBox = document.querySelector('.password-match');
+
 function checkPassword(){
-    const password = document.querySelector('#password');
-    const confirmPassword = document.querySelector('#confirm-password');
-    const messageBox = document.querySelector('.password-match');
     if(password.value !== confirmPassword.value){
-        password.style.cssText = 'box-shadow: 0 0 0 2pt red;';
-        confirmPassword.style.cssText = 'box-shadow: 0 0 0 2pt red;';
-        messageBox.textContent = '*Passwords do not match'
+        password.style.cssText = 'box-shadow: 0 0 0 2pt #c72132;';
+        confirmPassword.style.cssText = 'box-shadow: 0 0 0 2pt #c72132;';
+        messageBox.textContent = '*Passwords do not match';
+
+        password.addEventListener('keydown', removeFieldStyle);
+        confirmPassword.addEventListener('keydown', removeFieldStyle);
+
         return false;
     } else {
         return true;
     }
 
+}
+
+//removes the red outline when the user retypes their password
+function removeFieldStyle(){
+    password.style.cssText = '';
+    confirmPassword.style.cssText = '';
+    messageBox.textContent = '';
 }
